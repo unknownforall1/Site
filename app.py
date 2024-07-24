8e8fafd96fc99e70f4d1001065fac3f8e183e402
Vivek Raj Roy <vivekrajroy705@gmail.com>
2024-07-24 12:42:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,44 +20,43 @@ def search():
 
     video_url = result["result"][0]["link"]
     video_id = result["result"][0]["id"]
-    user_ip = request.remote_addr
-    user_download_dir = os.path.join('downloads', user_ip)
-    if not os.path.exists(user_download_dir):
-        os.makedirs(user_download_dir)
+    video_title = result["result"][0]["title"]
+    video_thumbnail = result["result"][0]["thumbnails"][0]["url"]
+    download_dir = 'downloads'
+    if not os.path.exists(download_dir):
+        os.makedirs(download_dir)
 
     ydl_opts = {
         'format': 'bestvideo+bestaudio/best',
-        'outtmpl': os.path.join(user_download_dir, '%(id)s.%(ext)s'),
+        'outtmpl': os.path.join(download_dir, '%(id)s.%(ext)s'),
     }
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(video_url, download=True)
             downloaded_file = ydl.prepare_filename(info_dict)
-            video_title = info_dict['title']
-            video_thumbnail = info_dict['thumbnail']
 
         video_url_path = os.path.basename(downloaded_file)
-        return redirect(url_for('play', user_ip=user_ip, video_id=video_id, video_title=video_title, video_thumbnail=video_thumbnail))
+        return redirect(url_for('play', video_id=video_id, video_title=video_title, video_thumbnail=video_thumbnail, video_file=video_url_path))
     except Exception as e:
         return f"Error downloading song: {e}", 500
 
-@app.route('/play/<user_ip>/<video_id>')
-def play(user_ip, video_id):
-    user_download_dir = os.path.join('downloads', user_ip)
-    video_files = [f for f in os.listdir(user_download_dir) if re.match(fr'{video_id}.*', f)]
+@app.route('/play/<video_id>')
+def play(video_id):
+    download_dir = 'downloads'
+    video_files = [f for f in os.listdir(download_dir) if re.match(fr'{video_id}.*', f)]
     if not video_files:
         abort(404)
 
     video_file = video_files[0]
-    video_path = os.path.join(user_download_dir, video_file)
+    video_path = os.path.join(download_dir, video_file)
     video_title = request.args.get('video_title')
     video_thumbnail = request.args.get('video_thumbnail')
     return render_template('play.html', video_path=video_path, video_title=video_title, video_thumbnail=video_thumbnail)
 
-@app.route('/watch/<user_ip>/<video_file>')
-def watch(user_ip, video_file):
-    directory = os.path.join('downloads', user_ip)
+@app.route('/watch/<video_file>')
+def watch(video_file):
+    directory = 'downloads'
     if not os.path.exists(os.path.join(directory, video_file)):
         abort(404)
     return send_from_directory(directory, video_file)
